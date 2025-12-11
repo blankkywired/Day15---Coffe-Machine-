a7ffa2c7609e78cba7ea74f2cd5806f28a109fe5
blankkywired <blankky@protonmail.com>
2025-12-10 21:59:22 -0300

    Feature: added options list

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 # first step
-
+resources_value = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+    "money": 0
+}
 coffes_options = [
     {
         "name": "espresso",
         "water": 50,
         "coffee": 18,
-        "price": 1.50
+        "money": 1.50
     },
 
     {
@@ -13,24 +18,37 @@ coffes_options = [
         "water": 200,
         "coffee": 24,
         "milk": 150,
-        "price": 2.50
+        "money": 2.50
     },
 
     {
-        "name": "cappucciono",
+        "name": "cappuccino",
         "water": 250,
         "coffee": 24,
         "milk": 100,
-        "price": 3.50
+        "money": 3.50
     }
 ]
+coffee_list = []
+# --------------------Adicionando opções de cafe para a lista ----------#
+for i in range(len(coffes_options)):
+    for j in coffes_options[i]:
+        if j == "name":
+            #print(coffes_options[i][j])
+            coffee_list.append(coffes_options[i][j])
 
-#Statement to print any components value
+        else:
+            continue
 
-#or i in coffees[1]:
-#    print(coffees[1][i])
+    
 
+userChoice = input('What would you like? (espresso/latte/cappuccino): ').lower()
+if userChoice == "report":
+    #Exibindo recursos
+    for i in resources_value:
+        print(f"{i.capitalize()}: {resources_value[i]}")
 
+# --------------------Exibindo catálogo ----------#
 for i in range(len(coffes_options)):
     for j in coffes_options[i]:
         print(f"{j}:", coffes_options[i][j])
