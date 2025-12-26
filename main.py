4302bd5884a58dea0da14a9c79c47171d4671a70
blankkywired <blankky@protonmail.com>
2025-12-26 07:38:53 -0300

    Added Docstring

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ coffes_options = [
     },
 
     {
-        "name": "late",
+        "name": "latte",
         "water": 200,
         "coffee": 24,
         "milk": 150,
@@ -48,6 +48,7 @@ for i in range(len(coffes_options)):
             continue
 
 def showResources_and_catalog(answer):
+    """Display machine resources to the user.Exibe resources"""
     if answer == "report":
         print('----------RESOURCES----------')
         for i in resources_InititalValues:
@@ -64,7 +65,9 @@ def showResources_and_catalog(answer):
             print('\n')
 
 #Implementar opção de aceitar moedas com base na escolha de cafe do usuario 
-def proccesCoins():
+def insert_coins():
+    """Colect coins of the user to procced with the buyment"""
+    print('Please insert coins')
     quarters = float(input('How many quarters?: ')) * 0.25
     dimes = float(input('How many dimes?: ')) * 0.10
     nickles = float(input('How many nickles?: ')) * 0.05
@@ -74,23 +77,31 @@ def proccesCoins():
     print(f"${totalSum:.2f}")
     return totalSum
 
+
 def main():
+ 
     userChoice = input('What would you like? (espresso/latte/cappuccino): ').lower()
     showResources_and_catalog(userChoice)
 
     if userChoice == "espresso" or userChoice == "latte" or userChoice == "cappuccino":
-        proccesCoins()
+        insert_coins()
+        check_value(insert_coins)
         return userChoice
+        
     
 def find_coffee_value():
+    """Find the price of coffee based on the user's choice."""
     choice = main() #Guardando escolha do usuario
     for coffee in coffes_options:
         for name in coffee:
             if coffee["name"] == choice:
                 valueCoffee = coffee["money"]
                 return valueCoffee
-print(find_coffee_value())
-                
+#print(find_coffee_value())
+def check_value(value):
+    if value < find_coffee_value():
+        print('Valor insuficiente')
+main()
 
 #TODOS: IMPLEMENTAR A FEATURE DE PEGAR O CAFE ESCOLHIDO PELO USUARIO E SUBTRAIR OS VALORES DOS RECURSOS NECESSARIOS
 # 2 - ADICIONAR FEATURE PARA ACEITAR MOEDAS DO USUARIO
