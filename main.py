b9dec5cdb520651a3a85afb00e9b50ef9eaa8a2d
blankkywired <blankky@protonmail.com>
2025-12-27 22:49:27 -0300

    feat: add make_coffee function

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-# first step
-resources_InititalValues = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-    "money": 0
-}
+
 # first step
 resources_InititalValues = {
     "water": 300,
@@ -38,7 +32,7 @@ coffee_options = {
         }
     }
 }
-
+revenue = 0
 
 def resources_catalog(answer):
     """Display machine resources to the user.Exibe resources"""
@@ -61,7 +55,9 @@ def resources_catalog(answer):
             print('\n')
 
 #Implementar opção de aceitar moedas com base na escolha de cafe do usuario 
+totalSum = 0
 def insert_coins():
+    global totalSum
     """Colect coins of the user to procced with the buyment"""
     print('Please insert coins')
     quarters = float(input('How many quarters?: ')) * 0.25
@@ -74,16 +70,7 @@ def insert_coins():
     #AMOUNT
     return totalSum
 
-choice = ""
-def main():
-    global choice
-    choice = input('What would you like? (espresso/latte/cappuccino): ').lower()
-    
-    if choice == "espresso" or choice == "latte" or choice == "cappuccino":
-        transaction(find_coffee_price(choice), insert_coins())        
-        return choice
-    elif choice == "report" or choice == "catalog":     
-        resources_catalog(choice)
+
 
 def find_coffee_price(choice):
     for coffee in coffee_options:
@@ -98,18 +85,42 @@ def find_coffee_price(choice):
 
 def transaction(coffe_choosen_price, amount):
     """Check if the buyment can be executed with successfully"""
-
+    
     if coffe_choosen_price > amount:
         print("Sorry, that's no enough money. Money refunded")
         print(f"Coffee value: {coffe_choosen_price}. Amount:{amount}")
-
+        return False
     else:
         #Troco do usuario
         if (amount - coffe_choosen_price) > 0:
             exchange = amount - coffe_choosen_price
             print(f"Here's your ${round(exchange)} in change")
         print(f"Here's your coffee. Enjoy your {choice} ☕!")
+        return True
        # making_coffee(choice)
-#def making_coffee(coffee):
-
+def resources_sufficient(coffee):
+    order = coffee_options[coffee]['ingredients']['cost']
+    for item in order:
+       if order[item] >  resources_InititalValues[item]:
+            print('Sorry, there is no more resources enough')
+            return False
+       
+def make_coffee(coffee, amount):
+    global revenue
+    order = coffee_options[coffee]['ingredients']
+    for item in order:
+        resources_InititalValues[item] = resources_InititalValues[item] - order[item]
+        revenue = coffee_options[coffee]['ingredients']['cost']
+choice = ""
+def main():
+    global choice
+    choice = input('What would you like? (espresso/latte/cappuccino): ').lower()
+    
+    if choice == "espresso" or choice == "latte" or choice == "cappuccino":
+        if transaction(find_coffee_price(choice), insert_coins()) == True:
+            make_coffee(choice, totalSum)        
+        return choice   
+    elif choice == "report" or choice == "catalog":     
+        resources_catalog(choice)
+    
 main()
